91641f93ee9b098b5f3351a0eeb863deca83162d
l2late <lucadelaat@gmail.com>
2020-05-14 15:15:58 +0200

    made helper function more easy to use

--- tclab/MATLAB/help_define_heater_input.m
+++ tclab/MATLAB/help_define_heater_input.m
@@ -7,41 +7,37 @@ time = 1:1:loops;
 Q1 = zeros(loops,1);    
 Q2 = zeros(loops,1);   
 
-% set interval begin and end as fraction of total time
-int_h1 = [.1 0.2;
-          0.2 0.5;
-          0.5 0.8;
-          0.8 1  ];
-
-indx_h1 = [floor(int_h1(:,1)*loops) ceil(int_h1(:,2)*loops)];
+% set heating values for heater 1
+% column 1 and 2: interval begin and end as fraction of total time
+% column 3: heater setting for the corresponding interval
+set_h1 =    [.1     .2      90;
+             .2     .5      30;
+             .5     .8      100;
+             .8     1       10];
 
 % adjust level for heater 1
-Q1(indx_h1(1,1):indx_h1(1,2),1) = 90;
-Q1(indx_h1(2,1):indx_h1(2,2),1) = 30;
-Q1(indx_h1(3,1):indx_h1(3,2),1) = 100;
-Q1(indx_h1(4,1):indx_h1(4,2),1) = 10;
-
-
-% set interval begin and end as fraction of total time
-int_h2 = [.01 0.1;
-          .1   .3;
-          0.3 0.6;
-          0.6 0.7;
-          0.7 1  ];
-
-indx_h2 = [floor(int_h2(:,1)*loops) ceil(int_h2(:,2)*loops)];
+for ii = 1:size(set_h1,1)
+    Q1(floor(set_h1(ii,1)*loops):ceil(set_h1(ii,2)*loops),1) = set_h1(ii,3);
+end
+    
+% set heating values for heater 2
+% column 1 and 2: interval begin and end as fraction of total time
+% column 3: heater setting for the corresponding interval
+set_h2 =    [.01    .1      40;
+             .1     .3      10;
+             .3     .6      60;
+             .6     .7      50;
+             .7      1      100];
 
 % adjust level for heater 2
-Q2(indx_h2(1,1):indx_h2(1,2),1) = 40;
-Q2(indx_h2(2,1):indx_h2(2,2),1) = 10;
-Q2(indx_h2(3,1):indx_h2(3,2),1) = 60;
-Q2(indx_h2(4,1):indx_h2(4,2),1) = 50;
-Q2(indx_h2(5,1):indx_h2(5,2),1) = 100;
+for ii = 1:size(set_h2,1)
+    Q2(floor(set_h2(ii,1)*loops):ceil(set_h2(ii,2)*loops),1) = set_h2(ii,3);
+end
 
 Q = [Q1 Q2];
 
 % plot heater input
-plot(time,Q) 
+plot(time,Q)
 ylabel('Heater setting');
 xlabel('Time (in seconds)');
 legend('Heater 1','Heater 2')
