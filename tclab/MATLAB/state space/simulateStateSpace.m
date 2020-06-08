6b0a38a887ea07dd8c2dfa5d3f291f6a0bde5b9c
l2late <lucadelaat@gmail.com>
2020-06-08 17:40:20 +0200

    greybox id working

--- tclab/MATLAB/state space/simulateStateSpace.m
+++ tclab/MATLAB/state space/simulateStateSpace.m
@@ -3,6 +3,7 @@ clear all; close all; clc;
 
 % load test data
 load ../results/luca/semi_random_test_60min_luca.mat
+%load ../results/halithan/semi_random_test_60min_halithan.mat
 
 % Extract data columns
 t       = data(:,1);
@@ -17,7 +18,7 @@ Tnom = mean([T1meas(1),T2meas(1)]) + 273.15;
 % Get state space system for linearized 2nd order physics model
 linsys = stateSpaceModel(Tnom);
 % load subspace identified state space system
-load ../model_parameters/sid_parameters_luca.mat
+%load ../model_parameters/sid_parameters_luca.mat
 
 % Initial state
 %Tbar    = ones(4,1)*Tnom;
@@ -28,7 +29,7 @@ ns = length(t);
 
 % Simulate state space responses
 Tp_lin  = lsim(linsys,[Q1 Q2],t,T0);
-Tp_sid  = lsim(sidsys,[Q1 Q2],t,T0);
+%Tp_sid  = lsim(sidsys,[Q1 Q2],t,T0);
 
 % Plot results
 figure('Position', [10, 10, 750, 400])
@@ -36,7 +37,7 @@ subplot(3,1,1)
 plot(t/60.0,T1meas,'b-','LineWidth',2)
 hold on
 plot(t/60.0,Tp_lin(:,1),'r--','LineWidth',2)
-plot(t/60.0,Tp_sid(:,1),'b:','LineWidth',2)
+%plot(t/60.0,Tp_sid(:,1),'b:','LineWidth',2)
 ylabel('Temperature (degC)')
 legend('T_1 measured','T_1 optimized','Location','NEO')
 
@@ -44,7 +45,7 @@ subplot(3,1,2)
 plot(t/60.0,T2meas,'b-','LineWidth',2)
 hold on
 plot(t/60.0,Tp_lin(:,2),'r--','LineWidth',2)
-plot(t/60.0,Tp_sid(:,2),'b:','LineWidth',2)
+%plot(t/60.0,Tp_sid(:,2),'b:','LineWidth',2)
 ylabel('Temperature (degC)')
 legend('T_2 measured','T_2 optimized','Location','NEO')
 
