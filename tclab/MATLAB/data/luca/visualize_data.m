3451062f964d4b4c3179de7c1ab9ec1adef91d9d
l2late <lucadelaat@gmail.com>
2020-06-11 10:41:49 +0200

    fixed error where identification and validation input were the same. Now need to rerun test

---
+++ tclab/MATLAB/data/luca/visualize_data.m
@@ -0,0 +1,30 @@
+close all; clear all; clc
+
+
+datafiles = dir('../inputs/prbs*.mat');
+
+nf = length(datafiles);
+
+for ii = 1:2
+    
+    load([sprintf('%s',datafiles(ii).folder), filesep, sprintf('%s',datafiles(ii).name)]);
+    
+    if exist('data','var') == 1
+        % Extract data columns
+        t = data(:,1);
+        Q1 = data(:,2);
+        Q2 = data(:,3);
+        T1meas = data(:,4);
+        T2meas = data(:,5);
+
+        figure
+        plot(t,T1meas,t,T2meas)
+        title(sprintf('%s',datafiles(ii).name))
+        
+    elseif exist('Q','var') == 1
+        figure
+        plot(Q)
+        title(sprintf('%s',datafiles(ii).name))
+    end
+    
+end
--- tclab/MATLAB/identification/generate_prbs_input.m
+++ tclab/MATLAB/identification/generate_prbs_input.m
@@ -5,13 +5,16 @@
 % of sampling intervals for which the value of the signal does not change.
 % Thus, the generated signal is constant over intervals of length 1/B samples.
 
+close all; clear all; clc
 
 time = 60;              % simulation time in min
 n_samples = time*60;    % simulation time in seconds
 n_inputs = 2;
 band = [0 0.01];
+band_val = [0 0.012];
 range = [0 100];
 
+% generate identification input
 Q = idinput([n_samples n_inputs],'prbs', band, range);
 
 figure
@@ -21,5 +24,20 @@ title('Q1')
 subplot(2,1,2)
 plot(Q(:,2))
 title('Q2')
+sgtitle('Identification')
 
 save('../data/inputs/prbs_heater_input_60min','Q');
+
+% generate Validation input
+Q = idinput([n_samples n_inputs],'prbs', band_val, range);
+
+figure
+subplot(2,1,1)
+plot(Q(:,1))
+title('Q1')
+subplot(2,1,2)
+plot(Q(:,2))
+title('Q2')
+sgtitle('Validation')
+
+save('../data/inputs/prbs_heater_input_60min_val','Q');
